756a8e4b479547ae354734b7bf4a1315926e4a5a
ThaoNP <npthao1312@gmail.com>
2021-04-07 14:42:09 +0700

    Edit comment

--- test_main.py
+++ test_main.py
@@ -1,14 +1,14 @@
 # Test Case 1:
-# Input: 93
-# Output: “Change: 50 20 20 2 1”
+# Input: 123
+# Output: “one hundred and twenty-three”
 def test_main_1(capfd):
     main(123)
     out, err = capfd.readouterr()
     assert out == "one hundred and twenty-three\n"
 
 # Test Case 2:
-# Input: 113
-# Output: “Change: 100 10 2 2”
+# Input: 211
+# Output: “two hundred and eleven”
 def test_main_2(capfd):
     main(211)
     out, err = capfd.readouterr()
